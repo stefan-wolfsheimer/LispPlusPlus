5e03c8841d64f2b7e825c672734fc291c0d17573
Stefan Wolfsheimer <stefan.wolfsheimer@surfsara.nl>
2018-08-05 22:42:45 +0200

    refactor gc simulation

--- test_simul/test_gc_sim.cpp
+++ test_simul/test_gc_sim.cpp
@@ -28,7 +28,7 @@ The views and conclusions contained in the software and documentation are those
 of the authors and should not be interpreted as representing official policies,
 either expressed or implied, of the FreeBSD Project.
 ******************************************************************************/
-#include <simul/gc_sim.h>
+#include <lpp/simul/gc_sim.h>
 #include <catch.hpp>
 
 using GcSim = Lisp::GcSim;
