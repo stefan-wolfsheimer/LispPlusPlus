ccaec47f4f78bac08c88899993b56d56d53c672b
Stefan Wolfsheimer <stefan.wolfsheimer@gmail.com>
2020-10-24 16:18:00 +0200

    memory management: free cons data structures

--- lisp/core/gc.c
+++ lisp/core/gc.c
@@ -5,35 +5,67 @@
 
 #define DEFAULT_PAGE_SIZE 1024
 
-static lisp_gc_collectible_list_t * _new_gc_collectible_list(lisp_gc_color_t c)
+/*****************************************************************************/
+static lisp_gc_collectible_list_t * _new_gc_collectible_list(lisp_gc_color_t c,
+                                                             short int is_root)
 {
   lisp_gc_collectible_list_t * lst = MALLOC(sizeof(lisp_gc_collectible_list_t));
   if(lst)
   {
+    lst->is_root = is_root;
     lisp_init_dl_list(&lst->objects);
     lst->color = c;
   }
   return lst;
 }
 
-int lisp_init_gc(lisp_gc_t * gc)
+static int lisp_init_color_map(lisp_gc_color_map_t * map)
 {
-  gc->white_list = _new_gc_collectible_list(LISP_GC_WHITE);
-  gc->grey_list = _new_gc_collectible_list(LISP_GC_GREY);
-  gc->black_list = _new_gc_collectible_list(LISP_GC_BLACK);
-  gc->white_root_list = _new_gc_collectible_list(LISP_GC_WHITE_ROOT);
-  gc->grey_root_list = _new_gc_collectible_list(LISP_GC_GREY_ROOT);
-  gc->black_root_list = _new_gc_collectible_list(LISP_GC_BLACK_ROOT);
-  if(!gc->white_list ||
-     !gc->grey_list ||
-     !gc->black_list ||
-     !gc->white_root_list ||
-     !gc->grey_root_list ||
-     !gc->black_root_list)
+  if( (map->white = _new_gc_collectible_list(LISP_GC_WHITE, 0)) == NULL)
   {
     return LISP_BAD_ALLOC;
   }
+  if( (map->grey = _new_gc_collectible_list(LISP_GC_GREY, 0)) == NULL)
+  {
+    return LISP_BAD_ALLOC;
+  }
+  if( (map->black = _new_gc_collectible_list(LISP_GC_BLACK, 0)) == NULL)
+  {
+    return LISP_BAD_ALLOC;
+  }
+  if( (map->white_root = _new_gc_collectible_list(LISP_GC_WHITE, 1)) == NULL)
+  {
+    return LISP_BAD_ALLOC;
+  }
+  if( (map->grey_root = _new_gc_collectible_list(LISP_GC_GREY, 1)) == NULL)
+  {
+    return LISP_BAD_ALLOC;
+  }
+  if( (map->black_root = _new_gc_collectible_list(LISP_GC_BLACK, 1)) == NULL)
+  {
+    return LISP_BAD_ALLOC;
+  }
+  return LISP_OK;
+}
 
+static int lisp_free_color_map(lisp_gc_color_map_t * map)
+{
+  FREE(map->white_root);
+  FREE(map->grey_root);
+  FREE(map->black_root);
+  FREE(map->white);
+  FREE(map->grey);
+  FREE(map->black);
+  return LISP_OK;
+}
+
+int lisp_init_gc(lisp_gc_t * gc)
+{
+  int ret = lisp_init_color_map(&gc->cons_color_map);
+  if(ret != LISP_OK)
+  {
+    return ret;
+  }
   gc->cons_pages = NULL;
   lisp_init_dl_list(&gc->recycled_conses);
   gc->num_cons_pages = 0;
@@ -44,13 +76,17 @@ int lisp_init_gc(lisp_gc_t * gc)
 
 int lisp_free_gc(lisp_gc_t * gc)
 {
-  FREE(gc->white_list);
-  FREE(gc->grey_list);
-  FREE(gc->black_list);
-  FREE(gc->white_root_list);
-  FREE(gc->grey_root_list);
-  FREE(gc->black_root_list);
-  return LISP_OK;
+  int ret = lisp_free_color_map(&gc->cons_color_map);
+  size_t i;
+  for(i = 0; i < gc->num_cons_pages; i++)
+  {
+    FREE(gc->cons_pages[i]);
+  }
+  if(gc->cons_pages)
+  {
+    FREE(gc->cons_pages);
+  }
+  return ret;
 }
 
 int lisp_gc_set_cons_page_size(lisp_gc_t * gc, size_t page_size)
@@ -79,6 +115,7 @@ lisp_cons_t * lisp_gc_alloc_cons(lisp_gc_t * gc)
   if(gc->cons_pos >= gc->cons_page_size)
   {
     gc->current_cons_page = MALLOC((sizeof(lisp_dl_item_t) +
+                                    sizeof(lisp_collectible_object_t) +
                                     sizeof(lisp_cons_t)) * gc->cons_page_size);
     gc->cons_pages = REALLOC(gc->cons_pages,
                              sizeof(void*) * (gc->num_cons_pages + 1));
@@ -89,15 +126,23 @@ lisp_cons_t * lisp_gc_alloc_cons(lisp_gc_t * gc)
     gc->cons_pages[gc->num_cons_pages++] = gc->current_cons_page;
     gc->cons_pos = 0;
   }
-  return (lisp_cons_t*)(
-                        (char*) gc->current_cons_page +
-                        ((sizeof(lisp_dl_item_t) +
-                          sizeof(lisp_cons_t)) * gc->cons_pos++) +
-                        sizeof(lisp_dl_item_t));
+  return (lisp_cons_t*)( (char*) gc->current_cons_page +
+                         (( sizeof(lisp_dl_item_t) +
+                            sizeof(lisp_collectible_object_t) +
+                            sizeof(lisp_cons_t)) * gc->cons_pos++) +
+                         sizeof(lisp_dl_item_t) +
+                         sizeof(lisp_collectible_object_t));
+}
+
+lisp_cons_t * lisp_gc_alloc_root_cons(lisp_gc_t * gc)
+{
+  return NULL;
 }
 
 void lisp_gc_free_cons(lisp_gc_t * gc, lisp_cons_t * cons)
 {
   lisp_dl_list_append(&gc->recycled_conses,
-                      ((lisp_dl_item_t*)cons) - 1);
+                      (lisp_dl_item_t*) (((char*)cons) -
+                                         sizeof(lisp_collectible_object_t) -
+                                         sizeof(lisp_dl_item_t)));
 }
--- lisp/core/gc.h
+++ lisp/core/gc.h
@@ -5,19 +5,38 @@
 
 typedef enum lisp_gc_color_t
 { 
-  LISP_GC_WHITE,
-  LISP_GC_GREY,
-  LISP_GC_BLACK,
-  LISP_GC_WHITE_ROOT,
-  LISP_GC_GREY_ROOT,
-  LISP_GC_BLACK_ROOT 
+  LISP_GC_WHITE = 0,
+  LISP_GC_GREY = 1,
+  LISP_GC_BLACK = 2,
+  // todo remove this cases
+  LISP_GC_WHITE_ROOT = 3,
+  LISP_GC_GREY_ROOT = 4,
+  LISP_GC_BLACK_ROOT = 5
 } lisp_gc_color_t;
+
+#define LISP_GC_NUM_COLORS 6
 
 typedef struct lisp_gc_collectible_list_t
 {
   lisp_dl_list_t objects;
   lisp_gc_color_t color;
+  short int is_root;
+  struct lisp_gc_collectible_list_t * other_elements;
+  struct lisp_gc_collectible_list_t * grey_elements;
+  struct lisp_gc_collectible_list_t * to_elements;
 } lisp_gc_collectible_list_t;
+
+typedef struct lisp_gc_color_map_t
+{
+  lisp_gc_collectible_list_t * white;
+  lisp_gc_collectible_list_t * grey;
+  lisp_gc_collectible_list_t * black;
+
+  lisp_gc_collectible_list_t * white_root;
+  lisp_gc_collectible_list_t * grey_root;
+  lisp_gc_collectible_list_t * black_root;
+
+} lisp_gc_color_map_t;
 
 typedef struct lisp_collectible_object_t
 {
@@ -26,6 +45,9 @@ typedef struct lisp_collectible_object_t
 
 typedef struct lisp_gc_t
 {
+  lisp_gc_color_map_t cons_color_map;
+
+  /* cons memory */
   void ** cons_pages;
   void * current_cons_page;
   lisp_dl_list_t recycled_conses;
@@ -33,27 +55,44 @@ typedef struct lisp_gc_t
   size_t num_cons_pages;
   size_t cons_pos;
 
-
-  lisp_gc_collectible_list_t * white_list;
-  lisp_gc_collectible_list_t * grey_list;
-  lisp_gc_collectible_list_t * black_list;
-  lisp_gc_collectible_list_t * white_root_list;
-  lisp_gc_collectible_list_t * grey_root_list;
-  lisp_gc_collectible_list_t * black_root_list;
-
 } lisp_gc_t;
 
+/**
+ * Initialize garbage collector object
+ */
 int lisp_init_gc(lisp_gc_t * gc);
+
+/**
+ * Free garbage collector object allocated with lisp_init_gc
+ */
 int lisp_free_gc(lisp_gc_t * gc);
 
 /**
  * Set the size of the cons pages.
- * Return LISP_OK or LISP_INVALID
+ * @return LISP_OK or LISP_INVALID
  */
 int lisp_gc_set_cons_page_size(lisp_gc_t * gc, size_t page_size);
 
+/**
+ * Allocate a cons object.
+ *
+ * @return pointer to lisp_cons_t structure
+ */
 struct lisp_cons_t * lisp_gc_alloc_cons(lisp_gc_t * gc);
 
+/**
+ * Allocate a root cons object.
+ *
+ * @return pointer to lisp_cons_t structure
+ */
+struct lisp_cons_t * lisp_gc_alloc_root_cons(lisp_gc_t * gc);
+
+/**
+ * free lisp_cons_t object allocated lisp_gc_alloc_cons.
+ *
+ * @param gc garbage collector object
+ * @param cons object to be freed.
+ */
 void lisp_gc_free_cons(lisp_gc_t * gc, struct lisp_cons_t * cons);
 
 #endif
